ae707af59e9d015d951a381d32180c4cde628ed4
Kikoooohub <krastev182@alumnos.cei.es>
2025-01-17 14:47:08 +0100

    cambiar nombre carpeta acceso

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,37 +1,5 @@
 
 
-
-
-
-
-
-
-
-
-
-
-//Declarar variables
-/*const preguntas = document.querySelectorAll('.Container-pregunta');
-
-
-
-preguntas.forEach(pregunta => {
-    pregunta.addEventListener('click', () => {
-        const respuestaAsociada = pregunta.closest('.Container-pregunta');
-
-       //quitar el visible de todos
-        respuesta.forEach(element => {
-            element.classList.remove('visible');
-        });
-
-        //agregar el visible a respuesta
-        respuestaAsociada.classList.toggle('visible');
-    });
-})*/
-
-
-
-
 // Selecciona todas las preguntas
 const preguntas = document.querySelectorAll('.Container-pregunta');
 
